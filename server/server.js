fb5dc1e44d9b2ed02b70d05805912d2485c7ccf7
Chris Minnick <chris@minnick.com>
2024-05-10 15:51:09 -0400

    adding get and post routes and database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { MongoClient, ObjectId } from 'mongodb';
+
+dotenv.config();
+
+const url = process.env.MONGO_DB_URL;
+const kwackDb = process.env.MONGO_DB;
+const kwacksCollection = process.env.MONGO_DB_COLLECTION;
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get('/posts', (req, res) => {
-  res.json([
-    { id: 1, content: 'Why is my duck winning so much?', author: 'Ben' },
-    { id: 2, content: "Hooray! It's Maya day!", author: 'Maya G' },
-  ]);
+
+app.post('/posts', async (req, res) => {
+  // todo: check for undefined and reject if a field isn't provided.
+  const { id, content, author } = req.body;
+
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(kwackDb);
+    const collection = db.collection(kwacksCollection);
+    const result = await collection.insertOne({ id, content, author });
+    res.status(201).send(`{"_id":"${result.insertedId}}`);
+  } catch (err) {
+    console.error(`Error:`, err);
+    res.status(500).send("Couldn't create Kwack!");
+  }
+});
+
+app.get('/posts', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(kwackDb);
+    const collection = db.collection(kwacksCollection);
+
+    const result = await collection.find({}).toArray();
+    res.json(result);
+  } catch (err) {
+    console.error(`Error:`, err);
+    res.status(500).send("Couldn't get Kwacks!");
+  }
 });
 app.listen(3000, () => console.log('server started on port 3000'));
